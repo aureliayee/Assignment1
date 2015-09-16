b51432ffdc63170eb007dcc7039f4ce38a27f264
Aurelia Yee <aureliayee@Aurelias-MacBook-Pro.local>
2015-09-16 13:21:10 -0700

    This is Assignment 1

---
+++ script.js
@@ -0,0 +1,167 @@
+// STEP 1
+/*
+var someMonth;
+function theMonth;
+const currentMonth;
+var summerMonths;
+function myLibraryFunction;
+*/
+
+// STEP 2
+/*
+384 //numeric
+"Aurelia" //string
+true //boolean
+null //null
+*/
+
+// STEP 3
+/*var firstExpression = 9 + (4-5) * 10;
+var secondExpression = firstExpression + (radius * radius);*/
+
+//STEP 4
+/*var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralSource;
+var mayWeContactYou;*/
+
+//STEP 5 - Way 1
+/*
+var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralSource;
+var mayWeContactYou;
+
+
+firstName = "Aurelia";
+lastName = "Yee";
+address = "Clairemont Mesa";
+city = "San Diego";
+state = "CA";
+zipCode = "92117";
+yourAge = "25";
+referralSource = "friend";
+mayWeContactYou = true;*/
+
+//STEP 5 - Way 2
+/*var firstName = "Aurelia";
+var lastName = "Yee";
+var address = "Clairemont Mesa";
+var city = "San Diego";
+var state = "CA";
+var zipCode = "92117";
+var yourAge = "25";
+var referralSource = "friend";
+var mayWeContactYou = true;*/
+
+//STEP 5 - Way 3
+/*var firstName = "Aurelia", lastName = "Yee", address = "Clairemont Mesa", city = "San Diego", state = "CA", zipCode = "92117", yourAge = "25", referralSource = "friend", mayWeContactYou = true;*/
+
+//STEP 6
+/*var petAge = 3;
+var catName = "Gale";
+var testAnswer = true;
+window.console.log("My cat, " + catName + ", is " + petAge + " years old.");
+window.console.log("Is it " + testAnswer + " that " + catName + " likes to eat lettuce?");
+window.console.log(petAge + testAnswer);*/
+
+//STEP 7
+/*var fruit = "apple";
+window.console.log("The color of " + fruit + " is " + color + "." )
+var color = "red";*/
+
+//STEP 8
+/*var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+window.console.log(someString);*/
+
+//STEP 9
+/*var x = null;
+var y;
+window.console.log(x);
+window.console.log(y);*/
+
+//STEP 10
+/*window.console.log(typeof "Aurelia");
+window.console.log(typeof 74829);
+window.console.log(typeof true);
+window.console.log(typeof Aurelia);
+window.console.log(typeof null);*/
+
+//STEP 11
+/*window.alert("Hello " + "Aurelia Yee" + ", welcome to the JavaScript class!");*/
+
+//STEP 12
+/*var name = "Aurelia Yee";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+//STEP 13
+/*var name = "Aurelia Yee";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+//STEP 14
+/*var name = "Aurelia Yee";
+var course = "JavaScript";
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 15
+/*var name = window.prompt("What is your name?");
+var course = "JavaScript";
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 16
+/*var name = window.prompt("What is your name?");
+var course = window.prompt("What class are you taking?");
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 17
+/*var x = 10;
+var y = 20;
+window.console.log(x + y);*/
+
+//STEP 18
+/*var x = 20;
+x += 20;
+window.console.log(x);*/
+
+//STEP 19
+/*var x = 20;
+x *= 5;
+window.console.log(x);*/
+
+//STEP 20
+/*var x = 20 % 3;
+x /= 1;
+window.console.log(x);*/
+
+//STEP 21
+/*var x = 20;
+var y = 100;
+window.console.log(x < y && x !== y);*/
+
+//STEP 22
+/*var x = 45;
+var y = 395;
+window.console.log(x == y || x >= y);*/
+
+//STEP 23
+/*var widget = new Object();
+window.console.log(typeof (widget));*/
+
+//STEP 24
+/*var widget = new Object();
+window.console.log(widget instanceof Object);*/
+
+//STEP 25
+/*var widget = new Object();
+window.console.log(widget instanceof String);*/
